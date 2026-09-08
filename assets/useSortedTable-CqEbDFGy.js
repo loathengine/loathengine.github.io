e15fd8600a5a13ed952a4d31e8c32f797272ec61
loathengine <loathengine@gmail.com>
2026-09-07 21:39:21 -0400

    Automated sync/build: 2026-09-07 21:39:16

---
+++ assets/useSortedTable-CqEbDFGy.js
@@ -0,0 +1,1 @@
+import{Kt as e}from"./vendor-D3niqIbR.js";import{O as t}from"./unitConversions-ZX0pQI_n.js";function n(n,r,i){return e(()=>n.toArray().then(e=>{let{desc:t,...n}=i??{},a=t?-1:1;return e.sort((e,t)=>a*r(e).localeCompare(r(t),void 0,n))}),[n])??t}function r(n,r){return e(()=>n.toArray().then(e=>e.sort((e,t)=>r(e)-r(t))),[n])??t}export{r as n,n as t};
